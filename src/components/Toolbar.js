b9161bc42c4d29492364678285934c71803a1510
Ben Martin <bmartin2009@gmail.com>
2022-10-12 16:01:59 -0500

    Starting on basic site layout.

---
+++ src/components/Toolbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Toolbar() {
+  return (
+    <aside className='col-span-2 bg-gray-50'>
+      <div className='text-gray-800'>
+        Form goes here
+      </div>
+    </aside>
+  );
+};
+
+export default Toolbar;
