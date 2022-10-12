85936b11fcfcc5414abcb3f60ebd7d576d44ba87
Ben Martin <bmartin2009@gmail.com>
2022-10-12 12:25:32 -0500

    Initial setup of Express and script to run both servers.

--- api/app.js
+++ api/app.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes');
+
+// This is the auto-generated routing from Express, but I use a routes/index.js to handle routing.
+var testAPI = require('./routes/testAPI.js');
 
 var app = express();
 
@@ -20,15 +22,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// This is the auto-generated routing from Express, but I use a routes/index.js to handle routing.
+app.use('/test', testAPI);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
