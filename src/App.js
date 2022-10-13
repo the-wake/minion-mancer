346e749487fdca66e8f4a50c94c9fc0f861da10c
Ben Martin <bmartin2009@gmail.com>
2022-10-13 11:31:41 -0500

    Working on early input validation from external API call.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 import Header from './components/Header.js';
@@ -6,11 +6,28 @@ import Homepage from './pages/Homepage.js';
 
 function App() {
 
+  const [loaded, setLoaded] = useState(false);
   const [creatureNames, setCreatureNames] = useState([]);
 
   const getCreatureList = () => {
     const reqUrl = 'https://api.open5e.com/monsters';
-  }
+
+    fetch(reqUrl)
+      .then(res => res.json())
+      .then(data => {
+        data.results.map((creature) => {
+          const addCreature = creature.name;
+          setCreatureNames(creatureNames => [...creatureNames, addCreature]);
+        });
+        console.log(creatureNames);
+      });
+  };
+
+  if (loaded === false) {
+    setLoaded(true);
+    getCreatureList();
+  };
+
 
   return (
     <div className="App">
