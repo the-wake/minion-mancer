f2f9726f25cff11e2b09d1a7a685244c29d4cec6
Ben Martin <bmartin2009@gmail.com>
2022-10-13 11:45:00 -0500

    Setting up components to populate with renders from queries.

--- src/App.js
+++ src/App.js
@@ -6,27 +6,28 @@ import Homepage from './pages/Homepage.js';
 
 function App() {
 
-  const [loaded, setLoaded] = useState(false);
-  const [creatureNames, setCreatureNames] = useState([]);
+  // This framework is to allow for autocomplete of the search form based on the external API call, but it's hit some issues and I'm pausing on it for now.
+  // const [loaded, setLoaded] = useState(false);
+  // const [creatureNames, setCreatureNames] = useState([]);
 
-  const getCreatureList = () => {
-    const reqUrl = 'https://api.open5e.com/monsters';
+  // const getCreatureList = () => {
+  //   const reqUrl = 'https://api.open5e.com/monsters';
 
-    fetch(reqUrl)
-      .then(res => res.json())
-      .then(data => {
-        data.results.map((creature) => {
-          const addCreature = creature.name;
-          setCreatureNames(creatureNames => [...creatureNames, addCreature]);
-        });
-        console.log(creatureNames);
-      });
-  };
+  //   fetch(reqUrl)
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       data.results.map((creature) => {
+  //         const addCreature = creature.name;
+  //         setCreatureNames(creatureNames => [...creatureNames, addCreature]);
+  //       });
+  //       console.log(creatureNames);
+  //     });
+  // };
 
-  if (loaded === false) {
-    setLoaded(true);
-    getCreatureList();
-  };
+  // if (loaded === false) {
+  //   setLoaded(true);
+  //   getCreatureList();
+  // };
 
 
   return (
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Toolbar from '../components/Toolbar.js';
 
+import CardLayout from '../components/CardLayout.js';
+
 
 function Homepage() {
+
   return (
     <main>
       <div className='grid grid-cols-12'>
         <Toolbar />
         <div className='col-span-10 main-content'>
-          <h2>Homepage</h2>
-          <p>Here's the stuff for the homepage.</p>
+          <CardLayout />
         </div>
       </div>
     </main>
